aa8e4f9e219ce795ffd3d62c1dadd1238d7552c4
JirongKim <munja__@naver.com>
2023-04-25 01:32:38 +0900

    for presentation

---
+++ main.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.listen(3000, function () {
+    console.log("start! express server on port 3000");
+});
+
+app.get("/", function (req, res) {
+    res.send("<h1>hi ncuc friend!</h1>");
+});
